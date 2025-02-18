384e6eefa4579a1a4b9362b02db90adac8f5cd78
Javivu748 <Javivu9@gmail.com>
2025-02-18 14:25:24 +0100

    js

---
+++ ejemploJS1.js
@@ -0,0 +1,13 @@
+//calcular la media aritmetica
+let suma=0;
+numeros=new Array();
+
+for(let i=0;i>suma;i++){
+    suma+=numeros[i];
+}
+
+let media=suma/numeros.lenght;
+document.write("la media es" + media);
+
+
+
