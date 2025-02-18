0d613b992b83337132add4cee7858ffceef5ff14
Javivu748 <Javivu9@gmail.com>
2025-02-18 14:38:29 +0100

    js 4.1

---
+++ ejercicio4-2.js
@@ -0,0 +1,14 @@
+numeros=new Array();
+var numeros=[1,2,3,4,5,6,7,8,9]
+
+function parImpar(){
+
+if (numeros % 2==0) {
+    console.log("es par")
+}
+else{
+    console.log("es impar")
+}
+}
+
+console.log( numeros + numeros.parImpar)
